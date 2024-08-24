e82078b18a8bb324d3e98cf36d200c32330d0da0
AstolfoProto <tokiya.kagami@gmail.com>
2024-08-24 11:46:31 +0800

    feat: add click event on cards

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import './src/styles/style.scss';
+
+// DOMContentLoaded = DOM確定載入後執行
+document.addEventListener('DOMContentLoaded', () => {
+  // 選擇所有需要被監聽的對象
+  const productCards = document.querySelectorAll('.product-card');
+
+  // 移除class name的事件
+  const removeActiveClassFromAll = () => {
+    productCards.forEach(card => card.classList.remove('product-card--active'));
+  };
+  
+  // 點擊事件
+  const handleCardClick = (card) => {
+    removeActiveClassFromAll()
+    card.classList.add('product-card--active');
+    console.log('product-card--active');
+  }
+
+  // 監聽所有的productCards，並且點擊後執行handleCardClick
+  productCards.forEach(card => {
+    card.addEventListener('click', () => handleCardClick(card));
+  });
+
+});
